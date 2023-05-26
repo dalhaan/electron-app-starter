25199040f199f5965b8a73fa669df17f15c1f77b
Dallan Freemantle <dallan@alphero.com>
2023-05-26 21:54:43 +1200

    Update title bar window title

--- apps/electron-app/src/renderer/components/layout/title-bar/title-bar.tsx
+++ apps/electron-app/src/renderer/components/layout/title-bar/title-bar.tsx
@@ -3,9 +3,13 @@ import packageJson from "@/./../../package.json";
 import * as styles from "./title-bar.css";
 
 export function TitleBar() {
+  const windowTitle = document.head.querySelector("title")?.innerText;
+
   return (
     <header className={styles.titleBar}>
-      <span className={styles.title}>{packageJson.productName}</span>
+      <span className={styles.title}>
+        {windowTitle || packageJson.productName}
+      </span>
     </header>
   );
 }
